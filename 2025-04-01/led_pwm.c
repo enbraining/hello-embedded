472ce5d691a105d225b5c276a362eb333159b049
Donghak Kim <me@bricn.net>
2025-04-01 10:21:30 +0900

    Create led_pwm.c

---
+++ 2025-04-01/led_pwm.c
@@ -0,0 +1,28 @@
+#include <wiringPi.h>
+#include <softPwm.h>
+
+int main()
+{
+  int light = 1, count = 0;
+
+  wiringPiSetup();
+
+  pinMode(2, OUTPUT);
+
+  softPwmCreate(2, 0, 100);
+
+  while(1)
+  {
+    softPwmWrite(2, light);
+    delay(50);
+    
+    if(count < 100) light--;
+    else if(count >= 100) light--;
+
+    if(light == 0)
+    {
+      count = 0;
+      break;
+    }
+  }
+}
